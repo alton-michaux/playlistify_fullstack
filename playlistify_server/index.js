cc18bdea42605b1048af6e3070936950495b9369
Alton Michaux <alton.michaux@yahoo.com>
2023-04-16 16:24:19 -0400

    still figuring out project structure

--- playlistify_server/index.js
+++ playlistify_server/index.js
@@ -1,12 +1,14 @@
 // server/index.js
 require('dotenv').config()
+
 const express = require("express");
+const getToken = require("./auth/backgroundToken")
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/token", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
