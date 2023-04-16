e661f7db2ddc9d2f5ee82ce494d74285d8ed1478
Alton Michaux <alton.michaux@yahoo.com>
2023-04-16 01:19:38 -0400

    added dotenv to project

--- playlistify_server/index.js
+++ playlistify_server/index.js
@@ -1,10 +1,14 @@
 // server/index.js
-
+require('dotenv').config()
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
